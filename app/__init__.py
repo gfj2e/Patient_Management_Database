fb48d401b49c7d74b51fc420e03f942d375ed91d
gfj2e <gfj2e@outlook.com>
2025-09-18 09:14:55 -0500

    Connected to Google Cloud
    Set up a basic google cloud connection

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,14 @@
 from flask import Flask
 from .main.routes import main_bp 
 from .auth.routes import auth_bp
+from .database.connection import init_connection_engine, db
 
 def create_app():
     app = Flask(__name__, static_folder="../static")
     app.secret_key = "your_secret_key"
+    
+    init_connection_engine(app)
+    
     # register the blueprints with app 
     app.register_blueprint(main_bp)
     app.register_blueprint(auth_bp)
---
+++ app/database/connection.py
@@ -0,0 +1,32 @@
+from flask_sqlalchemy import SQLAlchemy
+from google.cloud.sql.connector import Connector
+
+connector = Connector()
+
+db = SQLAlchemy()
+
+def init_connection_engine(app):
+    app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://"
+    app.config['SQLALCHEMY_ENGINE_OPTIONS'] = {
+        "creator": lambda: connector.connect(
+            "indigo-idea-472512-b0:us-east1:patient-mgmt",
+            "pymysql",
+            user = "root",
+            password="Database_mgmt4560",
+            db = "patient_mgmt",
+            ip_type = "public"
+        ),
+    }
+    
+    db.init_app(app)
+    
+    with app.app_context():
+        try:
+            db.engine.connect()
+            app.logger.info("Successfully connected to database!")
+            print("Database connection successful!")
+            return db
+        except Exception as e:
+            app.logger.error(f"Database connection error: {str(e)}")
+            print(f"Failed to connect to database: {str(e)}")
+            return db
