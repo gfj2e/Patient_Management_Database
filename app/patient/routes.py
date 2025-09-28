fe4eb504224dbb4fd406feb4dfff3ac011ff6531
Github-user-2468 <karigan.stewart05@gmail.com>
2025-09-27 19:00:12 -0500

    Actually fixing the portal hidden button

--- app/patient/routes.py
+++ app/patient/routes.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, render_template
 
-patient_bp  = Blueprint("patient", __name__, template_folder="templates")
+patient_bp = Blueprint("patient", __name__, template_folder="templates")
 
 @patient_bp.route("/patient")
-def patient():
-    return render_template("patient_home.html")
+def patient_home():
+    return render_template("patient_home.html")  
