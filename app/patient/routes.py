be9ddc3c78622d30c25fd350a9ef97e3cdcd8dfb
Github-user-2468 <karigan.stewart05@gmail.com>
2025-09-28 11:07:40 -0500

    Added appointments tab

--- app/patient/routes.py
+++ app/patient/routes.py
@@ -5,3 +5,7 @@ patient_bp = Blueprint("patient", __name__, template_folder="templates")
 @patient_bp.route("/patient")
 def patient_home():
     return render_template("patient_home.html")  
+
+@patient_bp.route("/appointments")
+def appointments():
+    return render_template("appointments.html")  
