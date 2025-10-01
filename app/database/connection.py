46f8741e973bc8742ff7dbc149d30a718def2335
Anlee Nguyen <anleen4@gmail.com>
2025-10-01 17:11:17 -0500

    Testing Push with Changed Website Name

--- app/database/connection.py
+++ app/database/connection.py
@@ -9,12 +9,13 @@ db = SQLAlchemy()
 # Use your own login credentials to connect to your server
 def init_connection_engine(app):
     db_user = "root"                # Replace these credentials
-    db_password = "password"
+    db_password = "Hien2003!!"
     # db_password = "andy"
     db_name = "patient_mgmt"
     db_host = "localhost"
 
-    app.config['SQLALCHEMY_DATABASE_URI'] = f"mysql+pymysql://{db_user}:{db_password}@{db_host}/{db_name}"
+    # app.config['SQLALCHEMY_DATABASE_URI'] = f"mysql+pymysql://{db_user}:{db_password}@{db_host}/{db_name}"
+    app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://root:Hien2003!!@127.0.0.1:3306/patient_mgmt"
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     
     db.init_app(app)
