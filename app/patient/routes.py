3b1c37ea7bc0423018f65aee227d146babe15860
Github-user-2468 <karigan.stewart05@gmail.com>
2025-10-05 15:24:15 -0500

    Added more patient pages (lab results, billing, messages, and patient info)

--- app/patient/routes.py
+++ app/patient/routes.py
@@ -2,6 +2,7 @@ from flask import Blueprint, render_template, redirect, url_for, flash
 from flask_login import login_required, current_user
 from ..database.models import Patient_Login
 
+
 patient_bp = Blueprint("patient", __name__, template_folder="templates")
 
 @patient_bp.route("/patient")
@@ -22,3 +23,19 @@ def appointments():
 @patient_bp.route("/prescriptions")
 def prescriptions():
     return render_template("prescriptions.html")
+
+@patient_bp.route("/billing")
+def billing():
+    return render_template("billing.html")
+
+@patient_bp.route("/lab-results")
+def lab_results():
+    return render_template("lab_results.html")
+
+@patient_bp.route("/messages")
+def messages():
+    return render_template("messages.html")
+
+@patient_bp.route("/patient_info")
+def patient_info():
+    return render_template("patient_info.html")
