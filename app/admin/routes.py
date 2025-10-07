4f883a71181ce892fc35295a58caff46ef35cd75
Heather <heatherspixels@gmail.com>
2025-10-07 16:47:27 -0500

    cleaning up some code

--- app/admin/routes.py
+++ app/admin/routes.py
@@ -5,9 +5,6 @@ from ..database.connection import db
 from app.database.seed import SPECIALTIES
 from datetime import date, datetime
 
-
-
-
 admin_bp = Blueprint("admin", __name__, template_folder="templates")
 
 @admin_bp.route("/admin")
@@ -18,7 +15,6 @@ def admin_home():
         doctors_count = Doctor.query.count()
         # billing_pending_count = Billing.query.filter_by(status="Pending").count()
         billing_total_count = Billing.query.count()
-
 
         return render_template(
             "admin_home.html",
@@ -31,6 +27,11 @@ def admin_home():
         flash("You must be logged in as an admin to view this page.")
         return redirect(url_for("auth.login"))
 
+
+
+
+
+
 @admin_bp.route("/admin/patients")
 @login_required
 def admin_patients():
@@ -41,104 +42,26 @@ def admin_patients():
         flash("You must be logged in as an admin to view this page.")
         return redirect(url_for("auth.login"))
 
-@admin_bp.route("/admin/doctors")
-@login_required
-def admin_doctors():
-    if current_user.is_authenticated and isinstance(current_user, Admin_Login):
-        doctors = Doctor.query.all()
-        return render_template("admin_doctors.html", doctors=doctors,specialties=SPECIALTIES)
-    else:
-        flash("You must be logged in as an admin to view this page.")
-        return redirect(url_for("auth.login"))
-
-@admin_bp.route("/admin/billing")
-@login_required
-def admin_billing():
-    if current_user.is_authenticated and isinstance(current_user, Admin_Login):
-        billing_records = Billing.query.all()
-        patients = Patient.query.all()
-        return render_template("admin_billing.html", billing_records=billing_records, patients=patients)
-    else:
-        flash("You must be logged in as an admin to view this page.")
-        return redirect(url_for("auth.login"))
-
-@admin_bp.route("/admin/patients")
-@login_required
-def patients():
-    patients = Patient.query.all()
-    return render_template("admin_patients.html", patients=patients)
-
-
-@admin_bp.route("/add_doctor", methods=["POST"])
-def add_doctor():
-    first_name = request.form.get("first_name")
-    last_name = request.form.get("last_name")
-    specialty = request.form.get("specialty")
-    email = request.form.get("email")
-    phone = request.form.get("phone")
-    city = request.form.get("city")
-    state = request.form.get("state")
-
-    # Checkbox returns "on" if checked, or None if not
-    accepting_new = True if request.form.get("accepting_patients") == "on" else False
-
-    if not all([first_name, last_name, email]):
-        flash("First name, last name, and email are required.", "danger")
-        return redirect(url_for("admin.admin_doctors"))
-
-    new_doctor = Doctor(
-        first_name=first_name,
-        last_name=last_name,
-        specialty=specialty,
-        state=state,
-        city=city,
-        phone_number=phone,
-        email=email,
-        is_accepting_new_patients=accepting_new
-    )
-    db.session.add(new_doctor)
-    db.session.commit()
-
-    flash("Doctor added successfully!", "success")
-    return redirect(url_for("admin.admin_doctors"))
-
-
-@admin_bp.route("/delete_doctor/<int:doctor_id>", methods=["POST"])
-def delete_doctor(doctor_id):
-    doctor = Doctor.query.get_or_404(doctor_id)
-    db.session.delete(doctor)
-    db.session.commit()
-    flash("Doctor deleted successfully.", "info")
-    return redirect(url_for("admin.admin_doctors"))
-
 @admin_bp.route("/add_patient", methods=["POST"])
 def add_patient():
     first_name = request.form["first_name"]
     last_name = request.form["last_name"]
     email = request.form.get("email")
     phone = request.form.get("phone_number")
-    dob_str = request.form.get("dob")
     gender = request.form.get("gender")
     address = request.form.get("address")
-
-    # generate next patient_code
     last_patient = Patient.query.order_by(Patient.patient_id.desc()).first()
     next_id = (last_patient.patient_id + 1) if last_patient else 1
     patient_code = f"P{next_id:04d}"
-
-    # Convert DOB string to date
+    dob_str = request.form.get("dob")
     dob = datetime.strptime(dob_str, "%Y-%m-%d").date()
-
-    # Compute age
     today = date.today()
     age = today.year - dob.year - ((today.month, today.day) < (dob.month, dob.day))
-
-    # Default fallback values
     height = 0
     marriage_status = "Single"
     race = "OTHER_MIXED"
-
-    default_doctor = Doctor.query.first()
+    #default_doctor = Doctor.query.first()
+    default_doctor = Doctor.query.order_by(db.func.rand()).first()
     doctor_id = default_doctor.doctor_id if default_doctor else None
 
     new_patient = Patient(
@@ -169,6 +92,75 @@ def delete_patient(patient_id):
     db.session.commit()
     flash("Patient deleted successfully!", "success")
     return redirect(url_for("admin.admin_patients"))
+
+
+
+
+
+@admin_bp.route("/admin/doctors")
+@login_required
+def admin_doctors():
+    if current_user.is_authenticated and isinstance(current_user, Admin_Login):
+        doctors = Doctor.query.all()
+        return render_template("admin_doctors.html", doctors=doctors,specialties=SPECIALTIES)
+    else:
+        flash("You must be logged in as an admin to view this page.")
+        return redirect(url_for("auth.login"))
+    
+@admin_bp.route("/add_doctor", methods=["POST"])
+def add_doctor():
+    first_name = request.form.get("first_name")
+    last_name = request.form.get("last_name")
+    specialty = request.form.get("specialty")
+    email = request.form.get("email")
+    phone = request.form.get("phone")
+    city = request.form.get("city")
+    state = request.form.get("state")
+
+    accepting_new = True if request.form.get("accepting_patients") == "on" else False
+
+    if not all([first_name, last_name, email]):
+        flash("First name, last name, and email are required.", "danger")
+        return redirect(url_for("admin.admin_doctors"))
+
+    new_doctor = Doctor(
+        first_name=first_name,
+        last_name=last_name,
+        specialty=specialty,
+        state=state,
+        city=city,
+        phone_number=phone,
+        email=email,
+        is_accepting_new_patients=accepting_new
+    )
+    db.session.add(new_doctor)
+    db.session.commit()
+
+    flash("Doctor added successfully!", "success")
+    return redirect(url_for("admin.admin_doctors"))
+
+@admin_bp.route("/delete_doctor/<int:doctor_id>", methods=["POST"])
+def delete_doctor(doctor_id):
+    doctor = Doctor.query.get_or_404(doctor_id)
+    db.session.delete(doctor)
+    db.session.commit()
+    flash("Doctor deleted successfully.", "info")
+    return redirect(url_for("admin.admin_doctors"))
+
+
+
+
+
+@admin_bp.route("/admin/billing")
+@login_required
+def admin_billing():
+    if current_user.is_authenticated and isinstance(current_user, Admin_Login):
+        billing_records = Billing.query.all()
+        patients = Patient.query.all()
+        return render_template("admin_billing.html", billing_records=billing_records, patients=patients)
+    else:
+        flash("You must be logged in as an admin to view this page.")
+        return redirect(url_for("auth.login"))
 
 @admin_bp.route("/add_billing", methods=["POST"])
 def add_billing():
